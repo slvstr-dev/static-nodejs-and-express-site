1295c58a29a4eb34100224c84b107c258560fcdd
Sylvester Hofstra <hello@slvstr.dev>
2021-03-22 14:08:03 +0100

    Fix 404 favicon error.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const path = require("path");
 const express = require("express");
 const app = new express();
+const path = require("path");
+const favicon = require("serve-favicon");
 const routes = require("./routes/index");
 const port = 3000;
 
 // Set view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+// Set favicon directory
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 // Set static assets directory
 app.use("/static", express.static(path.join(__dirname, "public")));
